1ff5525d8f0eeacaba819d4e448a5f67b4fab95d
Henrique Gabriel <henriquegb152019@gmail.com>
2024-09-20 00:07:07 -0300

    Atualizado e pronto para implementar.

--- 19-09-2024/ProjetoNaoDeterministico/src/main/java/br/edu/ifgoiano/aluno/exercicio/dupla/PrincipalTest.java
+++ 19-09-2024/ProjetoNaoDeterministico/src/main/java/br/edu/ifgoiano/aluno/exercicio/dupla/PrincipalTest.java
@@ -1,7 +1,27 @@
 package br.edu.ifgoiano.aluno.exercicio.dupla;
 
+import org.json.JSONObject;
+
 public class PrincipalTest {
     public static void main(String[] args) {
+        String testeJson;
+
+        testeJson =  "{\n" +
+                "   \"id\": \"19292868552\",\n" +
+                "   \"name\": \"Facebook Platform\",\n" +
+                "   \"likes\": 4190683,\n" +
+                "   \"category\": \"Product/service\",\n" +
+                "   \"is_published\": true,\n" +
+                "   \"website\": \"http://developers.facebook.com\",\n" +
+                "   \"username\": \"platform\",\n" +
+                "   \"founded\": \"2007\",\n" +
+                "   \"mission\": \"To make the web more open and social.\",\n" +
+                "   \"talking_about_count\": 98577\n" +
+                "}";
+
+        JSONObject test = new JSONObject(testeJson);
+        String mostrar = test.getString("category");
+        System.out.println(mostrar);
 
     }
 }
